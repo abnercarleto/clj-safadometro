55b28af8789e1527636224e0313236140b3fbbda
Abner Carleto <abnercarleto@ymail.com>
2015-12-08 23:47:32 -0200

    testes

--- test/clj_safadometro/core_test.clj
+++ test/clj_safadometro/core_test.clj
@@ -14,3 +14,11 @@
 
   (testing "Should negative number as argumento to somatorio return 0"
     (is (= 0 (somatorio -20)))))
+
+(deftest test-calc-safadeza
+  (testing "Should calc safadeza"
+    (is (= 45.94 (calc-safadeza 91 5 16)))))
+
+(deftest test-calc-anjo
+  (testing "Should calc anjo from safadeza"
+    (is (= 54.06 (calc-anjo 45.94)))))
